eceb0129e647e093d2d35b084d7627419e0b8b24
Siddharth Kataria <32744185+SidKataria@users.noreply.github.com>
2019-10-24 22:34:08 -0600

    v2.0
    Added reflective ways to get object class name, superclass's name and implemented interfaces.

--- 501A2Reflection/src/Inspector.java
+++ 501A2Reflection/src/Inspector.java
@@ -1,12 +1,49 @@
-
+/**
+ * Inspector Class for Refection
+ * @author sid
+ *
+ */
 public class Inspector {
+	
+	public Inspector() {
+	}
 
     public void inspect(Object obj, boolean recursive) {
         Class c = obj.getClass();
+        System.out.println("");
         inspectClass(c, obj, recursive, 0);
     }
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
+    	Class superC = null;
+    	Class[] interfaces = null;
+    	int counter1 = 0;
+    	int counter2 = 0;
+
+        //1. Getting the name of the declaring class
+        System.out.println("Declaring class: " + c.getName());
+        
+        //2. Getting the name of the immediate superclass
+        if (c != null && c.getName() != "java.lang.Object") {
+        	superC = c.getSuperclass();
+        	System.out.println("	SuperClass: " + superC.getName());
+        }
+        
+        //2a. Getting superclasses if more
+        while (superC != null && superC.getName() != "java.lang.Object") {
+        	counter1++;
+        	superC = superC.getSuperclass();
+        	for (int i = -1; i <= counter1; i++)
+        		System.out.print("	");
+        	System.out.println("SuperClass: " + superC.getName());
+        }
+        
+        //3. Getting the name of interfaces implemented
+        interfaces = c.getInterfaces();
+        for (int i = 0; i < interfaces.length; i++) {
+        	counter2++;
+        	System.out.println("	Interface: " + interfaces[i]);
+        }
     }
 
 }
