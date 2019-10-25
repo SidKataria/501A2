7ab3a49e5f5d661dcea9e9aaaa5476f53defb5ab
Siddharth Kataria <32744185+SidKataria@users.noreply.github.com>
2019-10-25 13:29:18 -0600

    v4.0
    Incomplete introspection, methods and fields still needed. Refactoring methods applied: extract method, introduce new fields.

--- 501A2Reflection/src/Inspector.java
+++ 501A2Reflection/src/Inspector.java
@@ -1,16 +1,34 @@
 /**
+ * CPSC 501 Assignment 2
  * Inspector Class for Refection
- * @author sid
- *
+ * @author Siddharth Kataria
+ * UCID: 30000880
+ * Fall 2019
  */
+import java.lang.reflect.*;
+
 public class Inspector {
 	
-	private Class superC = null;
-	private Class[] interfaces = null;
-	private int counter1 = 0;
-	private int counter2 = 0;
+	private Class superC;
+	private Class[] interfaces;
+	private Constructor[] classConstructors;
+	private int modifier;
+	private String mod;
+	private Class[] paramTypes;
+	private Method[] methods;
+	private int counter1;
+	private int counter2;
 	
 	public Inspector() {
+		counter1 = 0;
+		counter2= 0;
+		classConstructors = null;
+		interfaces = null;
+		superC = null;
+		paramTypes = null;
+		modifier = -1;
+		mod = null;
+		methods = null;
 	}
 
     public void inspect(Object obj, boolean recursive) {
@@ -20,16 +38,51 @@ public class Inspector {
     }
 
     private void inspectClass(Class c, Object obj, boolean recursive, int depth) {
-        //1. Getting the name of the declaring class
+        //Getting the name of the declaring class
         getObjClass(c, recursive);
-        //2, 3. Getting the name of the immediate superclass and parent classes if any and implemented interfaces within
+        
+        //Getting the name of the immediate superclass and parent classes if any and implemented interfaces within
         getObjSuperClass(c, recursive);
+        
+        //Getting the constructors
     }
     
     //Method to get object's declaring class's name
     private void getObjClass(Class c, boolean recursive) {
-    	System.out.println("Declaring Class: \n " + c.getName());
+    	System.out.println("Declaring Class-> \n" + c + "\n name:" + c.getName());
+    	getClassConstructors(c, 1, recursive);
     	getInterfaces(c, recursive, 0);
+    }
+    
+    //Method to get a class's declared methods
+    private void getDecMethods(Class c, int counter, boolean recursive) {
+    	
+    }
+    
+    //Method to get Class Constructors
+    private void getClassConstructors(Class c, int counter, boolean recursive) {
+    	classConstructors = c.getConstructors();
+    	for (Constructor cc : classConstructors) {
+    		properPrint(counter, "Constructor->");
+    		System.out.println(cc);
+    		properPrint(counter, "none");
+    		System.out.println(" name: " + cc.getName());
+    		getParamTypes(cc, recursive, counter);
+    		mod = getModString(cc, recursive);
+    		properPrint(counter+1, "Modifier->");
+    		properPrint(counter-1, "none");
+    		System.out.println(" type: " + mod);
+    	}
+    }
+    
+    //Method to get Constructor's modifiers
+    private void getParamTypes(Constructor c, boolean recursive, int counter) {
+    	paramTypes = c.getParameterTypes();
+		for (int i = 0; i < paramTypes.length; i++) {
+			properPrint(counter+1, "ParameterType->");
+			properPrint(counter-1, "none");
+			System.out.println(" param" + "["+i+"]: " + paramTypes[i].getName());
+		}
     }
     
     //Method to get object's superclass
@@ -37,8 +90,11 @@ public class Inspector {
     	if (c != null && c.getName() != "java.lang.Object") {
     		counter2++;
         	superC = c.getSuperclass();
-        	properPrint(counter2, "SuperClass");
-        	System.out.println(superC.getName());
+        	properPrint(counter2, "SuperClass->");
+        	System.out.println(superC);
+        	properPrint(counter2, "none");
+        	System.out.println(" name: " + superC.getName());
+        	getClassConstructors(superC, counter2+1, recursive);
         	getInterfaces(superC, recursive, counter2);
         }
     	getSuperClasses(superC, recursive);
@@ -49,8 +105,11 @@ public class Inspector {
     	while (superC != null && superC.getName() != "java.lang.Object") {
         	counter1++;
         	superC = superC.getSuperclass();
-        	properPrint((counter1 + 1), "SuperClass");
-        	System.out.println(superC.getName());
+        	properPrint((counter1 + 1), "SuperClass->");
+        	System.out.println(superC);
+        	properPrint(counter1, "none");
+        	System.out.println(" name: " + superC.getName());
+        	getClassConstructors(superC, counter1, recursive);
         	getInterfaces(superC, recursive, counter1);
         }
     }
@@ -59,18 +118,54 @@ public class Inspector {
     private void getInterfaces(Class c, boolean recursive, int counter) {
     	interfaces = c.getInterfaces();
         for (int i = 0; i < interfaces.length; i++) {
-        	properPrint(counter + 1, "Interface");
-        	System.out.println(" " + interfaces[i].getName());
+        	properPrint(counter + 1, "Interface->");
+        	System.out.println(interfaces[i]);
+        	properPrint(counter + 1, "none");
+        	System.out.println(" name: " + interfaces[i].getName());
+        	getClassConstructors(interfaces[i], counter+1, recursive);
         }
     }
     
     //Method to print tabs accordingly
     private void properPrint(int counter, String info) {
+    	if (!info.equals("none")) {
+	    	for (int i = 0; i < counter; i++)
+	    		System.out.print("	");
+	    	System.out.println(info + " ");
+    	}
     	for (int i = 0; i < counter; i++)
     		System.out.print("	");
-    	System.out.println(info + ": ");
-    	for (int i = 0; i < counter; i++)
-    		System.out.print("	" + " ");
+    	System.out.print(" ");
     }
-
+    
+    //Converting int value of modifier to String without toString method
+    private String getModString(Constructor cc, boolean recursive) {
+    	modifier = cc.getModifiers();
+    	if (modifier == 1024)
+    		return "abstract";
+    	else if (modifier == 16)
+    		return "final";
+    	else if (modifier == 512)
+    		return "interface";
+    	else if (modifier == 256)
+    		return "native";
+    	else if (modifier == 2)
+    		return "private";
+    	else if (modifier == 4)
+    		return "protected";
+    	else if (modifier == 1)
+    		return "public";
+    	else if (modifier == 8)
+    		return "static";
+    	else if (modifier == 2048)
+    		return "strict";
+    	else if (modifier == 32)
+    		return "synchronized";
+    	else if (modifier == 128)
+    		return "transient";
+    	else if (modifier == 16)
+    		return "volatile";
+    	return "none";
+    }
+    
 }
